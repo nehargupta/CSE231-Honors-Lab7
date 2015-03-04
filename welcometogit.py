f9bfe5e86f5b7414507e6b9866a9abe082119ab1
Weston Shellhorn <shellho1@msu.edu>
2015-03-03 20:32:42 -0500

    Added name

---
+++ welcometogit.py
@@ -0,0 +1,9 @@
+print("Hello! Congratulations on managing to fork this code successfully! \
+Make a change, and add a print statement with your name. Tell everyone you \
+managed to make it here! Then, follow \
+the instructions to commit the change to your repository. And continue \
+with the rest of the tutorial.")
+#2+2=4
+print("Neha Gupta (aka the TA) was here")
+
+print("Weston Shellhorn")
